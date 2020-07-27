f6b033395e1adba7e25b50b7de4973e328b4d2aa
laude <timothe.laude@gmail.com>
2020-07-27 12:36:51 +0200

    demander si jouer

--- src/main.rs
+++ src/main.rs
@@ -6,34 +6,52 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("arguments sont {:?}", args);
-    let secret_number = rand::thread_rng().gen_range(0, 100);
-    println!("le nombre est {} ",secret_number);
-    println!("Guess the number!");
+    loop {
+        let mut reponse = String::new();
+        println!("Veux tu jouer ?");
+      
+      match  io::stdin()
+        .read_line(&mut reponse){
+            Ok(n) => {
+             
+                if(reponse.trim()=="oui"){
+                let secret_number = rand::thread_rng().gen_range(0, 100);
+                println!("le nombre est {} ",secret_number);
+                println!("Guess the number!");
 
-    loop{
-        println!("Please input your guess.");
+                loop{
+                    println!("Please input your guess.");
 
-        let mut guess = String::new();
-        io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-        println!("tu as tapé : {} ",guess);
-       
-       
-        let guess: u32 = match guess.trim().parse(){
-            Ok(num)=> num,
-            Err(ParseIntError)=>{ 
-                println!("erreur de saisie veuillez entrez un nombre svp !"); 
-                continue;
-            },
-        };
+                    let mut guess = String::new();
+                    io::stdin()
+                    .read_line(&mut guess)
+                    .expect("Failed to read line");
+                    println!("tu as tapé : {} ",guess);
         
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal =>{ println!("You win!");
-                                break;
-                            }
-        }
+       
+                let guess: u32 = match guess.trim().parse(){
+                    Ok(num)=> num,
+                    Err(ParseIntError)=>{ 
+                        println!("erreur de saisie veuillez entrez un nombre svp !"); 
+                        continue;
+                    },
+                };
+                
+                match guess.cmp(&secret_number) {
+                    Ordering::Less => println!("Too small!"),
+                    Ordering::Greater => println!("Too big!"),
+                    Ordering::Equal =>{ println!("You win!");
+                                        break;
+                                    }
+                    }
+         }
+    }else{
+        println!("au revoir");
+    break;
     }
+} Err(error)=>{
+    
+}
+    }
+}
 }
